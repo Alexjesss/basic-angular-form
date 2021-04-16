37df971ae8729409842b3b6bf5a80b12862d185a
Alexandra <banica@9221@hotmail.be>
2021-04-16 15:11:51 +0200

    adjusting nice to have delete, applied styling

--- form/src/app/app.component.ts
+++ form/src/app/app.component.ts
@@ -1,8 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { Friend } from './friend';
+import {Component, OnInit} from '@angular/core';
+import {Friend} from './friend';
 import {AddFriendService} from './add-friend.service';
 import {HttpHeaders} from '@angular/common/http';
-
 
 
 @Component({
@@ -19,32 +18,31 @@ export class AppComponent implements OnInit {
   getAllFriends = 'http://localhost:9000/allFriends';
 
   addFriend(): void {
-    this.addFriendService.postRequest(this.friendModel).subscribe(succes => this.getAllFriends,
-        error => console.log(error));
-
+    this.addFriendService.postRequest(this.friendModel).subscribe(succes => 'it works',
+      error => console.log(error));
+    this.fetchFriends();
   }
 
   public async fetchFriends(): Promise<any> {
     await fetch(this.getAllFriends, {method: 'get', headers: {'Content-Type': 'application/json'}})
-  .then(response => {
-      return response.json() as Promise<any>;
-    })
-  .then(response => {
-      return this.friendList = response;
+      .then(response => {
+        return response.json() as Promise<any>;
+      })
+      .then(response => {
+        return this.friendList = response;
       });
   }
 
   ngOnInit(): any {
-    this.fetchFriends()
-    .then(response => console.log(this.friendList));
   }
 
   constructor(
     private addFriendService: AddFriendService,
-  ) {}
+  ) {
+  }
 
-  deleteFriend(): void{
-
+  deleteFriend(email: string): void {
+    this.friendList = this.friendList.filter(friend => friend.email !== email);
   }
 
 }
